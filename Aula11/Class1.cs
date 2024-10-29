5e53ebaec0b344c0e07c37e747111095111ebdb9
Arianne de Cássia Souza Costa <ariannedecassiasouzacosta@gmail.com>
2024-10-28 21:50:12 -0300

    Exercicio1_Aula1
    Exercicio1_Aula1

---
+++ Aula11/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1_Aula11
+{
+    internal class Class1
+    {
+        public double alturaRetangulo;
+        public double larguraRetangulo;
+
+        public Class1(double Altura, double Largura)
+        {
+            alturaRetangulo = Altura;
+            larguraRetangulo = Largura;
+        }
+
+        public double calcArea()
+        {
+            return alturaRetangulo * larguraRetangulo;
+        }
+
+        public double calcPerimetro()
+        {
+            return (alturaRetangulo * 2) + (larguraRetangulo * 2);
+        }
+    }
+}
---
+++ Aula11/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1_Aula11
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Digite a altura do retângulo: ");
+            double Altura = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a largura do retângulo: ");
+            double Largura = Convert.ToDouble(Console.ReadLine());
+
+            Class1 retangulo = new Class1(Altura, Largura);
+
+            double area = retangulo.calcArea();
+            double perimetro = retangulo.calcPerimetro();
+
+            Console.WriteLine($"A área do retângulo é: {area}");
+            Console.WriteLine($"O perímetro do retângulo é: {perimetro}");
+
+            Console.ReadLine();
+        }
+    }
+}
