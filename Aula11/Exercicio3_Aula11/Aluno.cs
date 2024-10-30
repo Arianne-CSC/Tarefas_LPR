63c80ce0c8e6c541beb12499bc2116fd338bcada
Arianne de Cássia Souza Costa <ariannedecassiasouzacosta@gmail.com>
2024-10-29 22:19:15 -0300

    Exercicio3_Aula11
    Exercicio3_Aula11

---
+++ Aula11/Exercicio3_Aula11/Aluno.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio3_Aula11
+{
+    internal class Aluno
+    {
+        public string nome;
+        public int matricula;
+        public double nota1;
+        public double nota2;
+        public double nota_trabalho;
+
+        public Aluno (string nomeAluno, int matriculaAluno, double nota1Aluno, double nota2Aluno, double nota_trabalhoAluno)
+        {
+            nome = nomeAluno;
+            matricula = matriculaAluno;
+            nota1 = nota1Aluno;
+            nota2 = nota2Aluno;
+            nota_trabalho = nota_trabalhoAluno;
+        }
+
+        public double Media()
+        {
+            double mediaFinal = (((nota1 * 2.5) + (nota2 * 2.5) + (nota_trabalho * 2)) / 7);
+            return mediaFinal;
+        }
+
+        public double notaFinal()
+        {
+            double mediaAtual = Media();
+
+            if(mediaAtual >= 7)
+            {
+                return 0;
+            }
+            else
+            {
+                double notaNecessaria = 7 - mediaAtual;
+                return notaNecessaria;
+            }
+        }
+    }
+}
---
+++ Aula11/Exercicio3_Aula11/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio3_Aula11
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Digite o nome do aluno: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite a matrícula do aluno: ");
+            int matricula = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite a primeira nota de prova do aluno: ");
+            double nota1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a segunda nota de prova do aluno: ");
+            double nota2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite uma nota de trabalho do aluno: ");
+            double nota_trabalho = Convert.ToDouble(Console.ReadLine());
+
+            Aluno aluno = new Aluno(nome, matricula, nota1, nota2, nota_trabalho);
+
+            Console.WriteLine($"A média do aluno foi: {aluno.Media():F2}");
+
+            double notaFinal = aluno.notaFinal();
+            if (notaFinal > 0)
+            {
+                Console.WriteLine($"O aluno precisa de {notaFinal} pontos para passar");
+            }
+            else
+            {
+                Console.WriteLine("O aluno já passou e não precisa de prova final!");
+            }
+        }
+    }
+}
